5fa06b330b1e29003afd460b40f41f9502eaccf6
sana2024 <sanaomervi01@gmail.com>
2022-07-06 17:25:50 +0300

    change web reqest addapter

--- Assets/Scripts/NakamaScripts/NakamaConnection.cs
+++ Assets/Scripts/NakamaScripts/NakamaConnection.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Nakama;
+using UnityEngine.Networking;
 
 [CreateAssetMenu]
 public class NakamaConnection : ScriptableObject
