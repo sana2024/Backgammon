58f09979f8bef363596d4d250ebe922f02051033
sana2024 <sanaomervi01@gmail.com>
2022-09-09 15:26:02 +0300

    add google play file

---
+++ Assets/Scripts/GSPManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.UI;
+using UnityEngine.SocialPlatforms;
+
+public class GSPManager : MonoBehaviour
+{
+
+    private PlayGamesClientConfiguration clientConfigration;
+    public Text status;
+    public Text Description;
+ 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        CongigureGPGS();
+        SingintoGPGS(SignInInteractivity.CanPromptOnce,clientConfigration);    }
+
+internal void CongigureGPGS(){
+
+    clientConfigration= new PlayGamesClientConfiguration.Builder().Build();
+}
+
+internal void SingintoGPGS(SignInInteractivity Interactivity,PlayGamesClientConfiguration configuration)
+{
+
+configuration=clientConfigration;
+PlayGamesPlatform.InitializeInstance(configuration);
+PlayGamesPlatform.Activate();
+PlayGamesPlatform.Instance.Authenticate(Interactivity,(code)=>{
+status.text="Athenticating...";
+if(code==SignInStatus.Success){
+
+    status.text="Successfully Authenticated";
+    Description.text="hello"+Social.localUser.userName +"You have an Id of"+Social.localUser.id;
+}
+
+else{
+
+     status.text="Failed to Authenticate";
+    Description.text="Failed to Authenticate, reason for failure is "+ code;
+}
+});
+
+}
+
+
+public void BasicSigninBtn(){
+
+    SingintoGPGS(SignInInteractivity.CanPromptAlways,clientConfigration);
+}
+
+
+}
+
